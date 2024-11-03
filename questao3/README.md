eceb8c1cc9e1287bdf40b50cac3ff99795934eb4
igor-oliveirasilvestre <148503591+igor-oliveirasilvestre@users.noreply.github.com>
2024-11-03 13:23:51 -0300

    Add files via upload

--- questao3/README.md
+++ questao3/README.md
@@ -1,1 +1,15 @@
-# Para rodar o código da questao 3
+### Pré-requisitos
+
+- **python3**
+- **pycryptodome**: instalado por :
+```bash
+pip install pycryptodome
+```
+
+### Execução do código
+
+Para rodar o código capaz de realizar a decodificação da
+mensagem abra um terminal na pasta questao3 e chame:
+
+
+
---
+++ questao3/src/testeunitario.py
@@ -0,0 +1,42 @@
+import unittest
+from test_questao3 import decodificar_mensagem
+import binascii
+
+class TestDecodificacaoAES(unittest.TestCase):
+    def test_mensagem_decodificada(self):
+        # Teste com valores conhecidos
+        cifra_hex = "a57fd9725fb53c53d5bd0b56185da50f70ab9baea5a43523b76c03e3eb989a20"
+        chave = "thisisasecretkey"
+        mensagem_esperada = "Sistemas Embarcados"
+        
+        # Executando a decodificacao
+        mensagem_resultante = decodificar_mensagem(cifra_hex, chave)
+        
+        # Verificando se a mensagem decodificada corresponde ao esperado
+        self.assertEqual(mensagem_resultante, mensagem_esperada)
+
+    def test_chave_incorreta(self):
+        # Teste com chave incorreta
+        chave = "thisisasecretkey"
+        cifra_hex = "a57fd9725fb53c53d5bd0b56185da50f70ab9baea5a43523b76c03e3eb989a20"
+        chave_incorreta = "thisisasecretkee"
+        
+        # Executando a decodificacao com chave incorreta
+        mensagem_resultante = decodificar_mensagem(cifra_hex, chave_incorreta)
+
+        #mensagem_resultante = mensagem_resultante.rstrip(b"\x00").decode('utf-8').strip()
+        
+        # Verificando que a saida nao corresponde a mensagem esperada, confirmando que a chave esta errada
+        self.assertNotEqual(mensagem_resultante, "Sistemas Embarcados")
+
+    def test_cifra_invalida(self):
+        chave = "thisisasecretkey"
+        cifra_invalida = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
+        
+        with self.assertRaises(binascii.Error):
+            decodificar_mensagem(cifra_invalida, chave)
+
+
+# Executando os testes
+if __name__ == '__main__':
+    unittest.main()
